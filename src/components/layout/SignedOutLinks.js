69dbe9948131a9dc8652372a6a9ee379e79d8297
prakash <prakash@divergentcs.org>
2020-12-09 07:58:40 +0530

    first commit

---
+++ src/components/layout/SignedOutLinks.js
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+const SignedOutLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">Sign Up</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log In</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedOutLinks;
