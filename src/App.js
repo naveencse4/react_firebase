a020710dc59d1df06d087860ebfef353858c132c
prakash <prakash@divergentcs.org>
2020-12-10 17:15:55 +0530

    updated auth and projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 import Dashboard from "./components/dashboard/Dashboard";
 import Navbar from "./components/layout/Navbar";
+import CreateProject from "./components/projects/CreateProject";
 import ProjectDetails from "./components/projects/ProjectDetails";
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
         <Switch>
           <Route path="/" component={Dashboard} exact />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
