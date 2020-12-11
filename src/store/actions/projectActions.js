ad2c1364dbbf24d5629b4b83a36b78dc7d920045
prakash <prakash@divergentcs.org>
2020-12-11 09:41:53 +0530

    updated index

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,4 +1,23 @@
-export const createProject = (project) => async (dispatch) => {
+export const createProject = (project) => (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
   // make async call
-  dispatch({ type: "CREATE_PROJECT", payload: project });
+  const firestore = getFirestore();
+  firestore
+    .collection("projects")
+    .add({
+      ...project,
+      authorFirstName: "Net",
+      authorLastName: "Vague",
+      authorId: 12345,
+      createdAt: new Date(),
+    })
+    .then(() => {
+      dispatch({ type: "CREATE_PROJECT", payload: project });
+    })
+    .catch((error) => {
+      dispatch({ type: "CREATE_PROJECT_ERROR", error });
+    });
 };
